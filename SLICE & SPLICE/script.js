4fb9dfbf1211754bc9130eefba159d58a0c7a14d
ariffadhillah <ariffadhillah319@gmail.com>
2023-02-12 23:43:05 +0700

    Javascript lanjutan ( SLICE, SPLICE )

---
+++ SLICE & SPLICE/script.js
@@ -0,0 +1,19 @@
+// var arr = ['Arif', 'Fadhillah', 'Fadhil']
+
+// 4 splice
+// splice(indexAwal, mauDihapus)
+
+// arr.splice(2,0, 'dosy', 'fitri')
+
+// mengahpus mengugunakan spile
+// arr.splice(1,2, 'dosy', 'fitri')
+// console.log(arr)
+
+// 5 slice
+// slice(indexAwal, akhir)
+var arr = ['Arif', 'Fadhillah', 'Fadhil', 'fitri', 'dody']
+
+
+var arr2 = arr.slice(3,4)
+console.log(arr.join(' - '));
+console.log(arr2.join(' - '));
