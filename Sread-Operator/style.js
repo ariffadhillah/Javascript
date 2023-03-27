70b4a118480ed4eda88b51a9dfadac31fd3d40ed
ariffadhillah <ariffadhillah319@gmail.com>
2023-03-27 07:18:53 +0700

    latihan 1 menggunakan fungsi map pada Spread Operatot

---
+++ Sread-Operator/style.js
@@ -0,0 +1,19 @@
+// latihan 1
+
+const liMhs = document.querySelectorAll('li');
+
+
+// const mhs = [];
+
+// for (let i = 0; i < liMhs.length; i++ ) {
+//     mhs.push(liMhs[i].textContent);
+// }
+
+// console.log(mhs)
+
+
+// menggunakan fungsi map
+
+const mhs = [...liMhs].map( m => m.textContent );
+console.log(mhs)
+
