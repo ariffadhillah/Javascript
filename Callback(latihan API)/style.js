29de81205dadfa28c1a9588a8b25c30b0c7da3db
ariffadhillah <ariffadhillah319@gmail.com>
2023-04-10 22:50:33 +0700

    add cards

--- Callback(latihan API)/style.js
+++ Callback(latihan API)/style.js
@@ -3,6 +3,23 @@
 $.ajax({
     url: 'http://www.omdbapi.com/?apikey=9e96ad3b&s=avengers',
     success: results => {
-        console.log(results);
+        const movies = results.Search;
+        let cards ='';
+        movies.forEach (m => {
+            cards += ` <div class="col-md-4 my-5">
+            <div class="card">
+                <img src="" class="card-img-top">
+                <div class="card-body">
+                  <h5 class="card-title">Avengers</h5>
+                  <h6 class="card-subtitle mb-2 text-muted">2016</h6>
+                  <a href="#" class="btn btn-primary">Show Detail</a>
+                </div>
+              </div>
+        </div>`;
+        });
+        $('.movie-container').html(cards);
+    },
+    error: (e) => {
+        console.log(e.responseText);
     }
 });
